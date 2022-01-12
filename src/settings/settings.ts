639c0b5ae802506876f69fdec344fe99b6fe3863
Nathan <nathan.hansen@thetacore.com>
2022-01-11 21:07:41 -0700

    moar work

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -2,9 +2,15 @@ import { Implementation } from "../main_implementations/implementations"
 
 export default class Settings {
     impl:Implementation
-    load_args(cli_args){
+    implementation_name:string = "memory"
+    port:number = 8000
 
+    // #learning about dependency injection
+
+    private constructor(cli_args:object) {
+        Object.assign(this, cli_args);
     }
+
     get_impl() {
         return this.impl
     }
